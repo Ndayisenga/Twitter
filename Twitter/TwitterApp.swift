0d9e39765782377e0c653c7458f2f16410b89aa7
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-12 12:16:02 +0200

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Ndayisenga Jean Claude on 12/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
